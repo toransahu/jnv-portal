c92264b88cbe1982149e25e82736e70c757ef198
TORAN <toran.sahu@yahoo.com>
2018-01-26 00:53:57 +0530

    creates User model & inherited AbstractUser model

--- users/models.py
+++ users/models.py
@@ -1,15 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 HOUSES = ('Aravali', 'Shivalik', 'Nilgiri', 'Udaygiri')
 
 
 # Create your models here.
-class User(models.Model):
-    first_name = models.CharField(blank=False, null=False)
-    last_name = models.CharField(blank=False, null=False)
-    email_id = models.EmailField(blank=False, null=False, unique=True)
-    mobile_number = models.CharField(blank=True, null=True)
-    age = models.IntegerField(blank=False, null=False)
-    house = models.CharField(choices=HOUSES)
+class User(AbstractUser):
+    pass
+
+    # first_name = models.CharField(blank=False, null=False)
+    # last_name = models.CharField(blank=False, null=False)
+    # email_id = models.EmailField(blank=False, null=False, unique=True)
+    # mobile_number = models.CharField(blank=True, null=True)
+    # age = models.IntegerField(blank=False, null=False)
+    # house = models.CharField(choices=HOUSES)
 
