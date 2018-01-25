1aff9f3ac225d7ca9214c4fe50a3017d3eace400
TORAN <toran.sahu@yahoo.com>
2018-01-25 22:37:45 +0530

    added model user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+HOUSES = ('Aravali', 'Shivalik', 'Nilgiri', 'Udaygiri')
+
+
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(blank=False, null=False)
+    last_name = models.CharField(blank=False, null=False)
+    email_id = models.EmailField(blank=False, null=False, unique=True)
+    mobile_number = models.CharField(blank=True, null=True)
+    age = models.IntegerField(blank=False, null=False)
+    house = models.CharField(choices=HOUSES)
+
