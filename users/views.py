813a0b249956a4892164a1864cf0e0edbefa5cca
TORAN <toran.sahu@yahoo.com>
2018-01-26 00:31:21 +0530

    added class based views UserList, UserDetail

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from users.models import User
+from rest_framework import generics
+from users.serializers import UserSerializer
+
 
 # Create your views here.
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.all().filter(username=self.request.user)
