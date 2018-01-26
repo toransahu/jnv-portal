205505578279b9e49b5fcb3ac948b31cf3532fc0
TORAN <toran.sahu@yahoo.com>
2018-01-26 23:19:07 +0530

    definition changed: passing all validated_data to create

--- users/serializers.py
+++ users/serializers.py
@@ -6,8 +6,11 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
-        user = User(username=validated_data.get('username', None))
-        user.set_password(validated_data.get('password', None))
+        # user = User(
+        #     username=validated_data.get('username', None)
+        # )
+        # user.set_password(validated_data.get('password', None))
+        user = User.objects.create_user(**validated_data)
         user.save()
         return user
 
@@ -17,12 +20,15 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
                 instance.set_password(validated_data.get(field))
             else:
                 instance.__setattr__(field, validated_data.get(field))
-            instance.save()
-            return instance
+        instance.save()
+        return instance
 
     class Meta:
         model = User
-        fields = ('url', 'id', 'username', 'password', 'first_name', 'last_name', 'email')
+        fields = ('url', 'id', 'username',
+                  'password', 'first_name', 'last_name',
+                  'email',
+                  )
         extra_kwargs = {
             'url': {
                 'view_name': 'users:user-detail',
