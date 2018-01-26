2d9f1f47e0d4e2b5df5c0f2dd8d454814ce5f0a9
TORAN <toran.sahu@yahoo.com>
2018-01-26 23:22:12 +0530

    details not found bug fixed: set queryset instead defining get_queryset()

--- users/views.py
+++ users/views.py
@@ -3,14 +3,14 @@ from rest_framework import generics
 from users.serializers import UserSerializer
 
 
-# Create your views here.
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def get_queryset(self):
-        return User.objects.all().filter(username=self.request.user)
+    # def get_queryset(self):
+    #     return User.objects.all().filter(username=self.request.user)
