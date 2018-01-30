56a5a85fa3a3f32f927e841e0643031419218ff6
toransahu <toran.sahu@yahoo.com>
2018-01-30 18:31:28 +0530

    added for authentication

--- users/views.py
+++ users/views.py
@@ -1,16 +1,20 @@
 from users.models import User
 from rest_framework import generics
 from users.serializers import UserSerializer
+from rest_framework import permissions
+from users.permissions import IsOwnerOrReadOnly
 
 
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
     # def get_queryset(self):
     #     return User.objects.all().filter(username=self.request.user)
