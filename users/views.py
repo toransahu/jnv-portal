b17da1e1e13a9ccab35959205c00b9bfd47674c2
toransahu <toran.sahu@yahoo.com>
2018-01-30 19:39:38 +0530

    tried to alter auth

--- users/views.py
+++ users/views.py
@@ -9,12 +9,13 @@ class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
-
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     # def get_queryset(self):
     #     return User.objects.all().filter(username=self.request.user)
