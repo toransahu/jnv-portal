47701b24b9b894367d56f4dbd00a3d94bcba0968
TORAN <toran.sahu@yahoo.com>
2018-01-31 06:12:54 +0530

    trying to add authentication.TokenAuthentication

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from users.models import User
 from rest_framework import generics
 from users.serializers import UserSerializer
 from rest_framework import permissions
+from rest_framework import authentication
 from users.permissions import IsOwnerOrReadOnly
 
 
@@ -9,13 +10,18 @@ class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    authentication_classes = (authentication.SessionAuthentication, authentication.BasicAuthentication,authentication.TokenAuthentication )
+    # permission_classes = (permissions.IsAuthenticated,)
+
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    authentication_classes = (
+        authentication.SessionAuthentication, authentication.BasicAuthentication, authentication.TokenAuthentication)
+    permission_classes = (permissions.IsAuthenticated,)
 
     # def get_queryset(self):
     #     return User.objects.all().filter(username=self.request.user)
